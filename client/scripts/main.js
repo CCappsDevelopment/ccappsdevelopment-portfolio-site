191dfd13563e81bfff99572ddd58e2b6cc01cf4c
CCappsDevelopment <CCappsDev@gmail.com>
2017-06-27 02:44:25 -0500

    :art: More CSS Added

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -3,17 +3,31 @@ $(document).ready(function() {
     $("#landingInfo").fadeIn(1000);
     
     $(window).scroll(function() {
+        
         if ($(window).scrollTop() > 200) {
             $("#aboutHeader").fadeIn(500);
-            $("#block1").fadeIn(500);
-            $("#block2").fadeIn(750);
-            $("#block3").fadeIn(1000);
+            $("#block1").fadeIn(750);
+            $("#block2").fadeIn(1000);
+            $("#block3").fadeIn(1250);
         }
         else {
             $("#aboutHeader").fadeOut();
             $("#block1").fadeOut();
             $("#block2").fadeOut();
             $("#block3").fadeOut();
+        }
+        
+        if ($(window).scrollTop() > 900) {
+            $("#portfolioHeader").fadeIn(500);
+            $("#card01").fadeIn(500);
+            $("#card02").fadeIn(750);
+            $("#card03").fadeIn(1000);
+        }
+        else {
+            $("#portfolioHeader").fadeOut(500);
+            $("#card01").fadeOut();
+            $("#card02").fadeOut();
+            $("#card03").fadeOut();
         }
     });
     
